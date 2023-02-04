3103c010a3845e45ea0098612e7f952ac5e5f9bb
HsuQnian <1084795069@qq.com>
2023-02-04 09:40:46 +0800

    load

--- src/views/ChildPage/Notice.vue
+++ src/views/ChildPage/Notice.vue
@@ -1,11 +1,23 @@
 <script setup>
+import { ref } from "vue";
+let url = "https://hsuqnian.top/assets/resources/Notice/Notice";
+let [exeUrl, zipUrl] = [ref(""), ref("")];
+console.log(exeUrl, zipUrl);
+fetch("https://hsuqnian.top/assets/resources/Notice/Notice.json")
+  .then((res) => res.text())
+  .then((data) => {
+    const json = JSON.parse(data);
+    exeUrl.value = `${url}-${json.version}.exe`;
+    zipUrl.value = `${url}-${json.version}-win.zip`;
+  });
 </script>
 <template>
   <div class="notice">
     <h1>Notice</h1>
     <p>Notice</p>
-    <a href="../../../public/resources/Notice/Notice-1.0.4.exe">download</a>
+    <a :href="exeUrl">exe</a>
+    <br />
+    <a :href="zipUrl">zip</a>
   </div>
 </template>
-<style scoped>
-</style>
+<style scoped></style>
