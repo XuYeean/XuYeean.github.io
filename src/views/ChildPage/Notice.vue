357f14b141eb9288d72083100ae627e5513ae8c1
HsuQnian <1084795069@qq.com>
2023-02-10 22:36:57 +0800

    Delete compressed package and installation package

--- src/views/ChildPage/Notice.vue
+++ src/views/ChildPage/Notice.vue
@@ -3,18 +3,14 @@ import { ref } from "vue";
 const app = defineProps({
   name: String,
 });
-let url = "https://hsuqnian.top/assets/resources/Notice/Notice";
-let { exeUrl, zipUrl } = { exeUrl: ref(""), zipUrl: ref("") };
-console.log(exeUrl, zipUrl);
-fetch(`${url}.json`, {
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-  },
-})
+let url =
+  "https://gitee.com/YeeanXu/XuYeean.github.io/releases/download/Notice";
+let exeUrl = ref("");
+let zipUrl = ref("");
+fetch(`${url}.json`)
   .then((res) => res.text())
   .then((data) => {
     const json = JSON.parse(data);
-    console.log(json);
     exeUrl.value = `${url}-${json.version}.exe`;
     zipUrl.value = `${url}-${json.version}-win.zip`;
   });
