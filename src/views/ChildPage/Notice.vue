91ecbcefae3712d6f188dd46024184b12f855c1a
HsuQnian <57381605+XuYeean@users.noreply.github.com>
2023-02-17 01:56:01 +0800

    load

--- src/views/ChildPage/Notice.vue
+++ src/views/ChildPage/Notice.vue
@@ -4,25 +4,24 @@ const app = defineProps({
   name: String,
 });
 let url = ref("");
-let exeUrl = ref("");
-let zipUrl = ref("");
+let WinUrl = ref("");
+let MacUrl = ref("");
 fetch(`https://hsuqnian.top/assets/resources/Notice/Notice.json`)
   .then((res) => res.text())
   .then((data) => {
     const json = JSON.parse(data);
     url.value = `https://gitee.com/YeeanXu/XuYeean.github.io/releases/download/Noticev${json.version}/Notice`;
-
-    exeUrl.value = `${url.value}-${json.version}.exe`;
-    zipUrl.value = `${url.value}-${json.version}-win.zip`;
+    WinUrl.value = `${url.value}-${json.version}.exe`;
+    MacUrl.value = `${url.value}-${json.version}.dmg`;
   });
 </script>
 <template>
   <div class="notice">
     <h1>Notice</h1>
     <p>Notice</p>
-    <a :href="exeUrl">exe</a>
+    <a :href="WinUrl">Windows</a>
     <br />
-    <a :href="zipUrl">zip</a>
+    <a :href="MacUrl">Mac</a>
   </div>
 </template>
 <style scoped></style>
