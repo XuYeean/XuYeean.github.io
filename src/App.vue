4c88044ac0c5dfda63a15f1d20786d8ab9d08a3b
HsuQnian <57381605+XuYeean@users.noreply.github.com>
2023-02-20 13:28:34 +0800

    load

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,9 @@ window.downloadLink =
   <transition name="back">
     <back v-if="route.path != '/'" />
   </transition>
+  <video id="backVideo" loop muted autoplay>
+    <source src="../public/back.mp4" type="video/mp4" />
+  </video>
   <router-view v-slot="{ Component }">
     <transition :name="orSo" mode="out-in">
       <component :is="Component" />
@@ -27,6 +30,14 @@ window.downloadLink =
 </template>
 
 <style scoped>
+#backVideo {
+  height: 100%;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  top: 0;
+  z-index: -1;
+}
 .ToHome-enter,
 .ToHome-leave,
 .ToHome-enter-active,
